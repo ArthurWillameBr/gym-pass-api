60de0144180bf22dc1b59792fc5782fcb44e1ea5
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-03 15:20:26 -0300

    feat: add test to ensure a user can be created

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -5,7 +5,19 @@ import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-user
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 describe("Register Use Case", () => {
-   
+  it("should hash user password upon registration", async () => {
+    const UsersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(UsersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
   it("should hash user password upon registration", async () => {
     const UsersRepository = new InMemoryUsersRepository();
     const registerUseCase = new RegisterUseCase(UsersRepository);
@@ -40,6 +52,6 @@ describe("Register Use Case", () => {
         email,
         password: "123456",
       })
-    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
   });
 });
