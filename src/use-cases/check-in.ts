587c769049427c701dcbcb756c3c977f28a8fd20
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-17 15:36:39 -0300

    feat: Implement CheckInUseCase for checking in at a gym

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,30 @@
+
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "@/repositories/chek-ins-repository";
+
+interface CheckInUseCaseRequest {
+  userId: string;
+  gymId: string;
+}
+
+interface CheckInUseCaseResponse {
+    checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private CheckInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+   const checkIn = await this.CheckInsRepository.create({
+    gym_id: gymId,
+    user_id: userId,
+   })
+
+    return {
+      checkIn
+    }
+  }
+}
