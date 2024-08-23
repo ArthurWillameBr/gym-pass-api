502aa10d63e1321261e836d61b04f564e62e3a77
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-22 21:24:49 -0300

    feat: Add validation to prevent multiple check-ins on the same day

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -28,21 +28,38 @@ describe("Check-in Use Case", () => {
     expect(checkIn.id).toEqual(expect.any(String));
   });
 
-  it("should not be able to check in twice in the same day", async () => {
-    
-    // garantir que os dois check-ins sejam feitos no mesmo dia
-    vi.setSystemTime(new Date("2024, 0, 20 8, 0, 0"));
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
 
     await sut.execute({
-      gymId: "gym-id",
-      userId: "user-id",
-    });
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
 
     await expect(() =>
       sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it("should be able to check in twice but in different days", async () => {
+    
+    vi.setSystemTime(new Date(2024, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymId: "gym-id",
+      userId: "user-01",
+    });
+
+    vi.setSystemTime(new Date(2024, 0, 21, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
         gymId: "gym-id",
-        userId: "user-id",
+        userId: "user-01",
       })
-    ).rejects.toBeInstanceOf(Error);
+    
+      expect(checkIn.id).toEqual(expect.any(String));
   });
 });
