2626e2bb9898185d204b3e0219d1f4062eb58097
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-22 21:50:26 -0300

    feat: add distance calculation between user and gym in CheckInUseCase

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,10 +1,14 @@
 
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInUseCaseRequest {
   userId: string;
   gymId: string;
+  userLatitude: number
+  userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -12,12 +16,25 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private CheckInsRepository: CheckInsRepository) {}
+  constructor(
+    private CheckInsRepository: CheckInsRepository,
+    private GymsRepository: GymsRepository
+  ) {}
+
+
 
   async execute({
     userId,
     gymId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.GymsRepository.findById(gymId)
+
+    if(!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    // calculate distance between user and gym
+    
 
     const checkInOnSameDate = await this.CheckInsRepository.findByUserIdOnDate(userId, new Date())
 
