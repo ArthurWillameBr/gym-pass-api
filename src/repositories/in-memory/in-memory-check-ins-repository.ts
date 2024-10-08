bac9e191a2f65e1072042d793001c85302a11087
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-08 17:47:51 -0300

    feat: add findById and save methods to InMemoryCheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -22,8 +22,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     if (!checkInOnSameDate) {
       return null;
     }
-
     return checkInOnSameDate;
+  }
+
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+    return checkIn;
   }
 
   async findManyByUserId(userId: string, page: number) {
@@ -49,4 +57,13 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkIn;
   }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if(checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
+    return checkIn
+  };
 }
