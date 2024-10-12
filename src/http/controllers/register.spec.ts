9b3dc2d49c4e6a148b1f3c1ab3cf14c8acee607c
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-12 16:28:15 -0300

    feat: add E2E test for user registration

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,21 @@
-import { test } from "vitest"
-test("ok", () => {
-})
+import requests from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Register (E2E)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should register a new user", async () => {
+    const response = await requests(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+    expect(response.statusCode).toEqual(201);
+  });
+});
