80d54f152a141ae04d60a98cb6aa535678976b99
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-12 16:10:22 -0300

    feat: add database setup and teardown for E2E testing

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,36 @@
-import { Environment } from "vitest"
+import { randomUUID } from "node:crypto";
+import { Environment } from "vitest";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+  }
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+  return url.toString();
+}
 
 export default <Environment>{
-    name: "prisma",
-    transformMode: 'web',
-    async setup() {
-        console.log("Setup")
+  name: "prisma",
+  transformMode: "web",
+  async setup() {
+    const schema = randomUUID();
+    const databaseULR = generateDatabaseURL(schema);
+    process.env.DATABASE_URL = databaseULR;
 
-        return {
-            teardown() {
-                console.log("Teardown")
-            },
-        }
-    }
-}
+    execSync(`npx prisma migrate deploy`);
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
+      },
+    };
+  },
+};
