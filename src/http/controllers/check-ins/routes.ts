d863ce7fbafa31782cef4a3b744adccc7469791f
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-16 16:31:16 -0300

    feat: add check-ins and gyms routes to include role-based authorization control

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,12 +4,13 @@ import { create } from "./create";
 import { validate } from "./validate";
 import { metrics } from "./metrics";
 import { history } from "./history";
+import { verifyUserRole } from "@/http/middlewares/verify-user-role";
 
 export async function checkInsRoutes(app: FastifyInstance) {
     app.addHook("onRequest", VerifyJWT)
     
     app.post("/gyms/:gymId/check-ins", create)
-    app.patch("/check-ins/:checkInId/validate", validate)
+    app.patch("/check-ins/:checkInId/validate", {onRequest: [verifyUserRole("ADMIN")]}, validate)
     app.get("/check-ins/metrics", metrics)
     app.get("/check-ins/history", history)
 
